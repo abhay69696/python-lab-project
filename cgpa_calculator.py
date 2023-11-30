669a4437c7500d2612f00590962dfd3e65a1e3c7
abhay69696 <abhaykiranak47@gmail.com>
2023-11-30 15:05:06 +0530

    Update cgpa_calculator.py

--- cgpa_calculator.py
+++ cgpa_calculator.py
@@ -2,5 +2,15 @@ mse1=input('enter mse1 marks')
 mse2=input('enter mse2 marks')
 internal=input('enter internal  marks') 
 cgpa=input('enter req cgpa')
-tot=mse1+mse2+internal
-print(tot)
+CGPA=int(cgpa)
+tot=int(mse1)+int(mse2)+int(internal)
+if(tot<=50):
+
+    see=(CGPA*10)-(tot)
+    if(see<=50):
+       print('req marks ins SEE=',see)
+    else:
+        print('not possible to get desired cgpa')
+
+else:
+    print("internal marks not entered correctly")
